b529c0db34cdc85292b67130827e641d91fdf78e
crazykushu <34454011+crazykushu@users.noreply.github.com>
2020-03-19 15:23:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,76 +11,76 @@ using namespace std;
 #define c 0.3197
 #define r 0.3868
 
-
-int * array_constructor(int * &intPtr, int &size, int a, int b )
+int *array_constructor(int *&intPtr, int &size, int a, int b)
 {
-   if (size <= 0)
-   {
-      intPtr = nullptr;
-      return intPtr;
-   }
-   else
-   {
-      default_random_engine generator; 
-      uniform_int_distribution<int> distribution(a, b);
-      intPtr = new int [size];
-	      for(int i = 0; i < size; i++)
-	      {
-		      intPtr[i] = distribution(generator) ; //uniform distribution of a random number
-	      }
-      distribution.reset();  
-	    return intPtr;
-   }
+    if (size <= 0)
+    {
+        intPtr = nullptr;
+        return intPtr;
+    }
+    else
+    {
+        default_random_engine generator;
+        uniform_int_distribution<int> distribution(a, b);
+        intPtr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            intPtr[i] = distribution(generator); //uniform distribution of a random number
+        }
+        distribution.reset();
+        return intPtr;
+    }
 }
 
 double seekTime(int position)
 {
-  double result;
-  if (position < divider)
-  {
-    result = t + c * pow((position - 1), r);
-  }
-  else
-  {
-    result = (c * r *(position - divider)) / pow((divider - 1), (1 - r)) + t + c * pow((divider - 1), r);
-  }
+    double result;
+    if (position < divider)
+    {
+        result = t + c * pow((position - 1), r);
+    }
+    else
+    {
+        result = (c * r * (position - divider)) / pow((divider - 1), (1 - r)) + t + c * pow((divider - 1), r);
+    }
 
-  return result;
+    return result;
 }
 
 int findNext(int *&intPtr, int currentPosition, int size)
 {
-  int minimum = 9000, index, difference;
-  for (int i = 0; i < size;i++)
-  {
-    difference = abs(currentPosition - intPtr[i]);
-    if (minimum > difference)
+    int minimum = 9000, index, difference;
+    for (int i = 0; i < size; i++)
     {
-      minimum = difference;
-      index = i;
+        difference = abs(currentPosition - intPtr[i]);
+        if (minimum > difference)
+        {
+            minimum = difference;
+            index = i;
+        }
     }
-  }
-  return index;
+    return index;
 }
 
-int main() {
-  int * intPtr = nullptr; //array pointer
-  int currentIndex;
+int main()
+{
+    int *intPtr = nullptr; //array pointer
+    int currentIndex;
 
-  int mincyl = 1;
-	for (int size = 1; size < 21; size++)
-  {
-    double totalTime = 0.000;
-    array_constructor(intPtr, size, mincyl, xmax);
-    int x = (rand() % (xmax - mincyl + 1)) + mincyl; //random initial position
-    cout << x << endl;
-    for (int i = 0; i < 10000; i++)
+    int mincyl = 1;
+    for (int size = 1; size < 21; size++)
     {
-      currentIndex = findNext(intPtr, x, size);
-      totalTime = totalTime + seekTime(intPtr[currentIndex]);
-      x = intPtr[currentIndex];
-      intPtr[currentIndex] = (rand() % (xmax - mincyl + 1)) + mincyl;
+        double totalTime = 0.000;
+        array_constructor(intPtr, size, mincyl, xmax);
+        int x = (rand() % (xmax - mincyl + 1)) + mincyl; //random initial position
+        cout << x << endl;
+        for (int i = 0; i < 10000; i++)
+        {
+            currentIndex = findNext(intPtr, x, size);
+            totalTime = totalTime + seekTime(intPtr[currentIndex]);
+            x = intPtr[currentIndex];
+            intPtr[currentIndex] = (rand() % (xmax - mincyl + 1)) + mincyl;
+        }
+        cout << "Average access time is " << totalTime / 10000 << "ms for a Q value of: " << size << endl;
     }
-    cout << "Average access time is " << totalTime / 10000 << "ms for a Q value of: " << size << endl;
-  }
 }
